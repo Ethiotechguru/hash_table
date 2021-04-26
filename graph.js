617e41000296b76d24d8128beb3705242bc67e61
Samuel Belete <samuelbelete@Samuels-MacBook-Pro.local>
2021-04-25 23:03:42 -0700

    DFS and BFS Traversing Recursive and Iterative solution

--- graph.js
+++ graph.js
@@ -8,42 +8,55 @@ class Graph{//undirected graph implementation only
         }
     }
     addEdges(ver1, ver2){
-        if (!this.adjacencyList[ver1]){
-            this.addVertex(ver1);
-        }
-        if (!this.adjacencyList[ver2]) {
-            this.addVertex(ver2);
-        }
-        if (!this.adjacencyList[ver1].includes(ver2)) this.adjacencyList[ver1].push(ver2);
-        if (!this.adjacencyList[ver2].includes(ver1)) this.adjacencyList[ver2].push(ver1)
+        this.addVertex(ver1);
+        this.addVertex(ver2)
+        this.adjacencyList[ver1].push(ver2);
+        this.adjacencyList[ver2].push(ver1)
+        // if (!this.adjacencyList[ver1]){
+        //     this.addVertex(ver1);
+        // }
+        // if (!this.adjacencyList[ver2]) {
+        //     this.addVertex(ver2);
+        // }
+        // if (!this.adjacencyList[ver1].includes(ver2)) this.adjacencyList[ver1].push(ver2);
+        // if (!this.adjacencyList[ver2].includes(ver1)) this.adjacencyList[ver2].push(ver1);
         
     }
-    removeEdge(ver1, ver2){
-        let idxVer2 = this.adjacencyList[ver1].indexOf(ver2);
-        let idxVer1 = this.adjacencyList[ver2].indexOf(ver1);
-        this.adjacencyList[ver2].splice(idxVer1, 1)
-        this.adjacencyList[ver1].splice(idxVer2, 1)
+    removeEdge(ver1, ver2){//removing Edges
+        // let idxVer2 = this.adjacencyList[ver1].indexOf(ver2);
+        // let idxVer1 = this.adjacencyList[ver2].indexOf(ver1);
+        // this.adjacencyList[ver2].splice(idxVer1, 1)
+        // this.adjacencyList[ver1].splice(idxVer2, 1)
+        if(!(this.adjacencyList[ver1] && this.adjacencyList[ver2])){
+            console.log(ver1, 'and', ver2, 'are not edges to each other');
+            return;
+        }
+        this.adjacencyList[ver1]=this.adjacencyList[ver1].filter(v => v !== ver2);
+        this.adjacencyList[ver2]=this.adjacencyList[ver2].filter(v => v !== ver1);
+
     }
     removeVertex(ver){
-        for(let i = 0; i<this.adjacencyList[ver].length; i++){
-            let value = this.adjacencyList[ver].splice(i, 1);
-            this.removeEdge(ver, value)
+        if(!this.adjacencyList[ver]){
+            console.log(ver, 'is not found!');
+            return;
         }
+        while(this.adjacencyList[ver].length>0){
+            let value = this.adjacencyList[ver][0];
+            this.removeEdge(ver, value);
+        }
+        delete this.adjacencyList[ver];
     }
 }
 
 let list = new Graph();
-list.addVertex('Tokyo');
-list.addEdges('Tokyo', 'New York')
-list.addEdges('Tokyo', 'California')
-// list.addVertex('New York')
-// list.addEdges('New York', 'Tokyo');
-list.addEdges('New York', 'Addis Ababa')
-list.addEdges('Addis Ababa', 'DC');
-list.addEdges('DC', 'Addis Ababa');
-list.addEdges('Paris', 'Addis Ababa');
-list.addEdges('Addis Ababa', 'Paris')
-list.addEdges('Cap Town', 'Addis Ababa');
-list.removeEdge('New York', 'Addis Ababa');
+list.addEdges('Tokyo', 'Dallas');
+list.addEdges('Tokyo', 'Addis Ababa');
+list.addEdges('New York', 'Tokyo');
+list.addEdges('California', 'Addis Ababa');
+list.addEdges('Tokyo', 'California');
+list.addEdges('California', 'New York');
+list.removeEdge('Tokyo', 'Dallas');
 list.removeVertex('Tokyo')
+list.removeEdge('Addis Abab', 'Cairo');
+list.removeVertex('Cairo');
 console.log(list.adjacencyList);
